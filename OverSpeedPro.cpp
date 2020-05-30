d1060001ce9e9d0f92180d724a0c94e023426831
Dewey <380350751@qq.com>
2020-05-30 23:08:47 +0800

    update WindowsSize
    使窗口在启动时设置为最大化。

--- OverSpeedPro.cpp
+++ OverSpeedPro.cpp
@@ -122,7 +122,7 @@ BOOL COverSpeedProApp::InitInstance()
 		return FALSE;
 
 	// 唯一的一个窗口已初始化，因此显示它并对其进行更新
-	m_pMainWnd->ShowWindow(SW_SHOW);
+	m_pMainWnd->ShowWindow(SW_SHOWMAXIMIZED);
 	m_pMainWnd->UpdateWindow();
 	return TRUE;
 }
